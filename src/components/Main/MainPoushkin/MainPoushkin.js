e191d8b01a18dd7aaeb033ad836d77b278bf12a0
Anna Kesova <IZABELE-TAN4@yandex.ru>
2023-02-01 00:11:35 +0300

    poushkin photo

--- src/components/Main/MainPoushkin/MainPoushkin.js
+++ src/components/Main/MainPoushkin/MainPoushkin.js
@@ -4,7 +4,19 @@ import "../MainPoushkin/MainPoushkin.css";
 function MainPoushkin() {
   return (
     <main className="page__content content">
-      
+      <section className="content__about-menu about-menu">
+        <div className="about-menu__name">
+          <div className="about-menu__poushkin">
+            <h2 className="about-menu__title">Трактир «Пушкин»</h2>
+            <div className="about-menu__fix">
+              <p className="about-menu__price">₽₽₽ • Европейская</p>
+              <div className="about-menu__black">
+                <p className="about-menu__time">40 - 50 мин</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
