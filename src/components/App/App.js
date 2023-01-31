242878572b1ee0514b73e22c7b35bf5fca08153c
Anna Kesova <IZABELE-TAN4@yandex.ru>
2023-01-31 23:18:05 +0300

    poushkin route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,24 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "../App/app.css";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Main from "../Main/Main";
+import MainPoushkin from "../Main/MainPoushkin/MainPoushkin";
 
 function App() {
   return (
     <div className="app">
       <div className="page">
         <Header />
-        <Main />
+        <Routes>
+          <Route exact path="/"
+          element={<Main/>}>   
+          </Route>
+           <Route exact path="/poushkin" 
+           element={<MainPoushkin/>}> 
+           </Route> 
+        </Routes>
         <Footer />
       </div>
     </div>
---
+++ src/components/Main/MainPoushkin/MainPoushkin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../MainPoushkin/MainPoushkin.css";
+
+function MainPoushkin() {
+  return (
+    <main className="page__content content">
+      
+    </main>
+  );
+}
+
+export default MainPoushkin;
