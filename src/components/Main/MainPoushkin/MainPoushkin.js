33d674b8471d539c8844ad47f95fa47de154593c
Anna Kesova <IZABELE-TAN4@yandex.ru>
2023-02-02 10:31:30 +0300

    без адаптива 2 страница

--- src/components/Main/MainPoushkin/MainPoushkin.js
+++ src/components/Main/MainPoushkin/MainPoushkin.js
@@ -1,5 +1,11 @@
 import React from "react";
 import "../MainPoushkin/MainPoushkin.css";
+import captureOne from "../../../images/Capture (3).svg";
+import captureTwo from "../../../images/Capture (2).svg";
+import captureThree from "../../../images/Capture (1).svg";
+import captureFour from "../../../images/Capture (4).svg";
+import captureFive from "../../../images/Capture (5).svg";
+import captureSix from "../../../images/Capture (6).svg";
 
 function MainPoushkin() {
   return (
@@ -19,12 +25,82 @@ function MainPoushkin() {
       </section>
       <section className="content__dish dish">
         <div className="dish__menu">
-            <p className="dish__filter">Закуски</p>
-            <p className="dish__filter">Салаты</p>
-            <p className="dish__filter">Супы</p>
-            <p className="dish__filter">Горячие блюда</p>
-            <p className="dish__filter">Гарниры</p>
-            <p className="dish__filter">Десерты</p>
+          <p className="dish__filter">Закуски</p>
+          <p className="dish__filter">Салаты</p>
+          <p className="dish__filter">Супы</p>
+          <p className="dish__filter">Горячие блюда</p>
+          <p className="dish__filter">Гарниры</p>
+          <p className="dish__filter">Десерты</p>
+        </div>
+      </section>
+      <section className="content__snacks snacks">
+        <div className="snacks__container">
+          <h2 className="snacks__title">Закуски</h2>
+          <ul className="snacks__list">
+            <li className="snacks__link">
+              <div className="snacks__about">
+                <div className="snacks__composition">
+                  <h3 className="snacks__subtitle">
+                    Сельдь на бородинском хлебе
+                  </h3>
+                  <p className="snacks__description">С яйцом и огурцом</p>
+                </div>
+
+                <p className="snacks__price">240 ₽</p>
+              </div>
+              <img className="snacks__photo" src={captureOne} alt=""></img>
+            </li>
+            <li className="snacks__link">
+              <div className="snacks__about">
+                <div className="snacks__composition">
+                  <h3 className="snacks__subtitle">Соленья ассорти</h3>
+                  <p className="snacks__description"></p>
+                </div>
+                <p className="snacks__price">320 ₽</p>
+              </div>
+              <img className="snacks__photo" src={captureTwo} alt=""></img>
+            </li>
+            <li className="snacks__link">
+              <div className="snacks__about">
+                <div className="snacks__composition">
+                  <h3 className="snacks__subtitle">Грибы маринованные</h3>
+                  <p className="snacks__description"></p>
+                </div>
+                <p className="snacks__price">300 ₽</p>
+              </div>
+              <img className="snacks__photo" src={captureThree} alt=""></img>
+            </li>
+            <li className="snacks__link">
+              <div className="snacks__about">
+                <div className="snacks__composition">
+                  <h3 className="snacks__subtitle">Сало домашнее с горчицей</h3>
+                  <p className="snacks__description"></p>
+                </div>
+                <p className="snacks__price">320 ₽</p>
+              </div>
+              <img className="snacks__photo" src={captureFour} alt=""></img>
+            </li>
+            <li className="snacks__link">
+              <div className="snacks__about">
+                <div className="snacks__composition">
+                  <h3 className="snacks__subtitle">Малосольная семга</h3>
+                  <p className="snacks__description"></p>
+                </div>
+                <p className="snacks__price">390 ₽</p>
+              </div>
+              <img className="snacks__photo" src={captureFive} alt=""></img>
+            </li>
+            <li className="snacks__link">
+              <div className="snacks__about">
+                <div className="snacks__composition">
+                  <h3 className="snacks__subtitle">Язык говяжий с хреном</h3>
+                  <p className="snacks__description"></p>
+                </div>
+                <p className="snacks__price">350 ₽</p>
+              </div>
+              <img className="snacks__photo" src={captureSix} alt=""></img>
+            </li>
+          </ul>
         </div>
       </section>
     </main>
